0634070519d01d5921517404a846636f114989be
victor <vili@nvidia.com>
2017-12-04 16:48:17 +0800

    add utils shell

---
+++ train_RaFD.sh
@@ -0,0 +1,4 @@
+python3 main.py --mode='train' --dataset='RaFD' --c_dim=8 --image_size=128 \
+                 --num_epochs=200 --num_epochs_decay=100 --sample_step=200 --model_save_step=200 \
+                 --sample_path='stargan_rafd/samples' --log_path='stargan_rafd/logs' \
+                 --model_save_path='stargan_rafd/models' --result_path='stargan_rafd/results'
