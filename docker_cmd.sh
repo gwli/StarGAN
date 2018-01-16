9fad8edb73f8ca943526bc468727337fbd7db542
victor <vili@nvidia.com>
2018-01-16 12:12:21 +0800

    git add debug config

--- docker_cmd.sh
+++
@@ -1,3 +0,0 @@
-docker pull nvdevtools/pytorch:0.2.0-cuda-8.0
-
-nvidia-docker run --privileged  -e DISPLAY  --net=host --ipc=host -it -v $HOME/.Xautority:/home/nvidia/.Xautoority -v `pwd`:/home/nvidia/samples/StartGAN nvdevtools/pytorch:0.2.0-cuda-8.0 /bin/bash
--- train_celebA.sh
+++
@@ -1,3 +0,0 @@
-python3 main.py --mode='train' --dataset='CelebA' --c_dim=5 --image_size=128 \
-                 --sample_path='stargan_celebA/samples' --log_path='stargan_celebA/logs' \
-                 --model_save_path='stargan_celebA/models' --result_path='stargan_celebA/results' 2>&1 |tee train_celebA_`date +%Y%m%d_%H%M%S`.log
