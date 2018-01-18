74b5a6fa8d0adebf962fbdc69b55e0b52578ee2a
victor <vili@nvidia.com>
2018-01-18 21:38:12 +0800

    fix the typo

--- devtools/docker_cmd.sh
+++ devtools/docker_cmd.sh
@@ -2,7 +2,7 @@ tag="nvdevtools/pytorch:0.2.0-cuda-8.0"
 
 nvidia-docker run --privileged  -e DISPLAY  --net=host --ipc=host -it --rm -p 5022:5022 -p 7022:22 \
      -v $HOME/.Xautority:/home/nvidia/.Xautoority \
-     -v /raid/dl_samples:/raid/samples \
+     -v /raid/dl_samples:/raid/dl_samples \
      -v /raid/tools:/raid/tools \
      -w `pwd`  \
      $tag /bin/bash
